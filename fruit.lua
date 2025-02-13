703d54fbb52d9275e127a40282389288679615d9
nguoitinhmuahe <tranthang20111@gmail.com>
2025-02-13 16:40:46 +0700

    Create fruit.lua

---
+++ fruit.lua
@@ -0,0 +1,129 @@
+repeat
+    wait()
+until game.IsLoaded and (game.Players.LocalPlayer or game.Players.PlayerAdded:Wait()) and
+    (game.Players.LocalPlayer.Character or game.Players.LocalPlayer.CharacterAdded:Wait())
+local l = true
+game.Players.LocalPlayer.Idled:connect(
+    function()
+        while wait(3) do
+            if l then
+                game.VirtualUser:Button2Down(Vector2.new(0, 0), workspace.CurrentCamera.CFrame)
+                wait(1)
+                game.VirtualUser:Button2Up(Vector2.new(0, 0), workspace.CurrentCamera.CFrame)
+            end
+        end
+    end
+)
+
+
+
+
+local CoreGui = game:GetService("StarterGui")
+    CoreGui:SetCore("SendNotification", {
+        Title = "Fruit Finder [Free]",
+        Text = "By _shinichi. discord.gg/wWX6EAYzrA",
+        Icon = "rbxthumb://type=Asset&id=15485121479&w=150&h=150",
+        Duration = math.huge,
+        Callback = AllowRunServiceBind,
+    })
+    
+  if Ran then
+    return
+else
+    getgenv().Ran = true
+end
+if game:GetService("Players").LocalPlayer.PlayerGui:WaitForChild("Main", 9e9):FindFirstChild("ChooseTeam") then
+    game:GetService("ReplicatedStorage").Remotes.CommF_:InvokeServer("SetTeam", "Pirates")
+    wait(3)
+end
+local a = game.Players.LocalPlayer
+local b = a.Character
+local c = game.TweenService
+local d = Instance.new("BodyVelocity")
+d.MaxForce = Vector3.new(1 / 0, 1 / 0, 1 / 0)
+d.Velocity = Vector3.new()
+d.Name = "bV"
+local e = Instance.new("BodyAngularVelocity")
+e.AngularVelocity = Vector3.new()
+e.MaxTorque = Vector3.new(1 / 0, 1 / 0, 1 / 0)
+e.Name = "bAV"
+for f, f in next, workspace:GetChildren() do
+    if f.Name:find("Fruit") and (f:IsA("Tool") or f:IsA("Model")) then
+        repeat
+            local d = d:Clone()
+            d.Parent = b.HumanoidRootPart
+            local e = e:Clone()
+            e.Parent = b.HumanoidRootPart
+            local a =
+                c:Create(
+                b.HumanoidRootPart,
+                TweenInfo.new((a:DistanceFromCharacter(f.Handle.Position) - 150) / 300, Enum.EasingStyle.Linear),
+                {CFrame = f.Handle.CFrame + Vector3.new(0, f.Handle.Size.Y, 0)}
+            )
+            a:Play()
+            a.Completed:Wait()
+            b.HumanoidRootPart.CFrame = f.Handle.CFrame
+            d:Destroy()
+            e:Destroy()
+            wait(1)
+        until f.Parent ~= workspace
+        wait(1)
+        local a =
+            b:FindFirstChildOfClass("Tool") and b:FindFirstChildOfClass("Tool").Name:find("Fruit") and
+            b:FindFirstChildOfClass("Tool") or
+            (function()
+                for a, a in a.Backpack:GetChildren() do
+                    if a.Name:find("Fruit") then
+                        return a
+                    end
+                end
+            end)()
+        print(a)
+        game:GetService("ReplicatedStorage").Remotes.CommF_:InvokeServer(
+            "StoreFruit",
+            a:GetAttribute("OriginalName"),
+            a
+        )
+    end
+end
+for a, a in pairs(game:GetService("Workspace"):GetChildren()) do
+    if a:IsA("Tool") and string.find(a.Name, "Fruit") then
+        NameFruit = a.Name
+    end
+end
+print(NameFruit)
+spawn(
+    function()
+        pcall(
+            function()
+                while wait(.1) do
+                    if _G.AutoStoreFruit then
+                        for a, a in pairs(NameFruit) do
+                            game:GetService("ReplicatedStorage").Remotes.CommF_:InvokeServer("StoreFruit", a)
+                        end
+                    end
+                end
+            end
+        )
+    end
+)
+local a = game.JobId
+repeat
+    local Http = game:GetService("HttpService")
+local TPS = game:GetService("TeleportService")
+local Api = "https://games.roblox.com/v1/games/"
+local _place = game.PlaceId
+local _servers = Api.._place.."/servers/Public?sortOrder=Asc&limit=100"
+function ListServers(cursor)
+   local Raw = game:HttpGet(_servers .. ((cursor and "&cursor="..cursor) or ""))
+   return Http:JSONDecode(Raw)
+end
+local Server, Next; repeat
+   local Servers = ListServers(Next)
+   Server = Servers.data[1]
+   Next = Servers.nextPageCursor
+until Server
+TPS:TeleportToPlaceInstance(_place,Server.id,game.Players.LocalPlayer)
+    wait()
+until game.JobId ~= a
+
